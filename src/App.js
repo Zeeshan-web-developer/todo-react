77966f20bf6c34825f85413389ddffeb6cba1dfc
zeeshan ahmad <zeeshan1361@gmail.com>
2021-03-30 21:18:49 +0530

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import db from "./firebase";
 
+import React, { useState, useEffect } from "react";
 function App() {
+  const [items, setItems] = useState(["zeeshan"]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    //it only loads first time
+    db.collection("todos").onSnapshot((snapshot) => {
+      setItems(snapshot.docs.map((doc) => doc.data().todo));
+    });
+  }, []);
+
+  const add = () => {
+    db.collection("todos").add({
+      todo: input,
+    });
+    setInput("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button onClick={add}>Add</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
-
 export default App;
